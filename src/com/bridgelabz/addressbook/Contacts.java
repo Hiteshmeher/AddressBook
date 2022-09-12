6a2951d7e023be691d8d84d3a9c4a5dc70ca42f8
Hiteshmeher <meher.hitesh4@gmail.com>
2022-09-12 10:01:15 +0530

    [Hitesh]: added code to ensure no Duplicate entry of the same person in particular Addressbook

---
+++ src/com/bridgelabz/addressbook/Contacts.java
@@ -0,0 +1,84 @@
+package com.bridgelabz.addressbook;
+
+/**
+ * 
+ * @author HITESH
+ *
+ */
+public class Contacts {
+	// local variables
+	String first_name, last_name, address, city, state, zip, phone, email;
+
+	// getters and setters
+	public String getFirstName() {
+		return first_name;
+	}
+
+	public void setFirstName(String first_name) {
+		this.first_name = first_name;
+	}
+
+	public String getLastName() {
+		return last_name;
+	}
+
+	public void setLastName(String lastName) {
+		this.last_name = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return phone;
+	}
+
+	public void setPhoneNumber(String phone) {
+		this.phone = phone;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	// to string method
+	@Override
+	public String toString() {
+		return ("First name: " + first_name + "\n Last name: " + last_name + "\n Address: " + address + "\n city: "
+				+ city + "\n state: " + state + "\n zip: " + zip + "\n phone number:" + phone + "\n email: " + email
+				+ "");
+	}
+}
